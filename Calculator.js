82ae59b284cb1c08998b0ef6d95e7470877b0706
Gilberto <gilbertogaspar1996@gmail.com>
2016-03-17 03:44:08 -0400

    Test

---
+++ Calculator.js
@@ -0,0 +1,15 @@
+/**
+ * Created by PC on 3/17/2016.
+ */
+function calculate() {
+    var wheatInput = document.getElementById("txtWheatInput");
+    var wheatOutput = document.getElementById("txtWheatOutput");
+    var mineralOutput = document.getElementById("txtMineralOutput");
+    var sugarOutput = document.getElementById("txtSugarOutput");
+    var agentOutput = document.getElementById("txtAgentOutput");
+    var wheat = Math.floor(wheatInput.value / 5) * 5;
+    wheatOutput.value = wheat;
+    mineralOutput.value = wheat * 6;
+    sugarOutput.value = wheat/5;
+    agentOutput.value = wheat*2;
+}
