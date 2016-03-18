9d6f06258cba14d00146759bda95f6324a9d1ef4
Nathan Perry <nbp8557@rit.edu>
2016-03-18 01:05:12 -0400

    Added the index for the node.js server to get started with. Also updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bdo-beer
+Requires NPM 3.0 or greater
+	 Express 4.13.3 or greater
+
+To run:
+
+	run "npm install"
+	run "node index.js"
+	
---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/Main.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
