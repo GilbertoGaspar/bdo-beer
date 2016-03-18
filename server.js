4e604cc6d06c26e39a616dba117c0f6092fbcb80
Nathan Perry <nbp8557@rit.edu>
2016-03-18 01:37:07 -0400

    Added a functionality to get application path on a Heroku VM

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//heroku VM local path
+process.env.PWD = process.cwd();
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.env.PWD + '/public'));
 
 // views is directory for all template files
 
